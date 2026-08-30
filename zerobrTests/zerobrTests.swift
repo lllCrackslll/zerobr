266f7902c132fa574850cd0dbd50237284b1a0b9
Rayane Batil <vortex14123@gmail.com>
2026-08-30 21:00:22 +0200

    Initial Commit

---
+++ zerobrTests/zerobrTests.swift
@@ -0,0 +1,19 @@
+//
+//  zerobrTests.swift
+//  zerobrTests
+//
+//  Created by Rayane Batil on 30/08/2026.
+//
+
+import Testing
+@testable import zerobr
+
+struct zerobrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
